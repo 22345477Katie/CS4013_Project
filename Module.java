7921dacfdb9fe74d85b7c09836506a6b54852851
22345477Katie <150158819+22345477Katie@users.noreply.github.com>
2023-11-14 16:53:49 +0000

    Update Module.java

--- Module.java
+++ Module.java
@@ -1,37 +1,51 @@
 public class Module {
-    int moduleId ;
-    double percentage;
-    String grade;
-    int semester;
-    int credits;
-    int duration;
-    String moduleName;
-    int[] gradeMarks;
-    String[] gradeTitles;
-    
-    public Module(String moduleName,int moduleId, int credits, int duration, int[] gradeMarks, String[] gradeTitles){
+    private int moduleId ;
+    private String grade;
+    private int semester;
+    private int credits;
+    private int duration;
+    private String moduleName;
+    private int[] gradeMarks;
+    private String[] gradeTitles;
+    private double nonQHrs;
+    private double QPV;
+
+    //Sophie
+    public Module(String moduleName,int moduleId, int credits, int duration, int[] gradeMarks, String[] gradeTitles, double nonQHrs, double QPV){
         this.moduleId = moduleId;
         this.credits = credits;
         this.duration = duration;
         this.moduleName = moduleName;
         this.gradeMarks = gradeMarks;
         this.gradeTitles = gradeTitles;
+        this.nonQHrs = nonQHrs;
+        this.QPV = QPV;
+        
     }
-    
+
+    //Sophie
     public String getModuleName(){
         return this.moduleName;
     }
-    
+
+    //Sophie
     public int getModuleId(){
         return this.moduleId;
     }
-    
+
+    //Sophie
     public int getDuration(){
         return this.duration;
     }
-    
+
+    //Sophie
     public int getCredits(){
         return this.credits;
+    }
+
+    //Katie
+    public int getQPV(){
+        return QPV;
     }
     
     
