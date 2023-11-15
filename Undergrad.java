b373f5eff9a7c7150d7af04f120b814283c1a36e
RaidMouras <133544926+RaidMouras@users.noreply.github.com>
2023-11-15 00:08:47 +0000

    Create Undergrad.java

---
+++ Undergrad.java
@@ -0,0 +1,38 @@
+public class Undergrad extends Program {
+    private int year;
+    private String course;
+
+    public Undergrad(String programId, String name, int duration) {
+        super(programId, name, "Undergraduate", duration);
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public void programProgression(Student student) {
+        int currentYear = student.getYearOfStudy();
+        if (currentYear < 4) {
+            System.out.println("Undergraduate progression to the next year.");
+            student.setYearOfStudy(currentYear + 1);
+        } else {
+            System.out.println("Student has completed the undergraduate program.");
+        }
+    }
+
+    public String programID() {
+        return getProgramId();
+    }
+}
