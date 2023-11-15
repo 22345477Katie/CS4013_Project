a30bac8982947099f6d86b20eb51e07170c80f93
RaidMouras <133544926+RaidMouras@users.noreply.github.com>
2023-11-15 00:09:57 +0000

    Create Masters.java

---
+++ Masters.java
@@ -0,0 +1,18 @@
+public class Masters extends PostgradProgram {
+    public Masters(String programId, String name, int duration) {
+        super(programId, name, duration);
+    }
+
+    public void programProgression(Student student) {
+        int currentYear = student.getYearOfStudy();
+        if (currentYear < 2) {
+            student.setYearOfStudy(currentYear + 1);
+        } else {
+            System.out.println("Student has completed the master's program.");
+        }
+    }
+
+    public String programID() {
+        return getProgramId();
+    }
+}
