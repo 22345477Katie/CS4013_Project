3ddadd0bfeb67874ff4105aa2d4d8b9d1b5921f5
RaidMouras <133544926+RaidMouras@users.noreply.github.com>
2023-11-15 00:10:58 +0000

    Create PhD.java

---
+++ PhD.java
@@ -0,0 +1,28 @@
+public class Phd extends PostgraduateProgram {
+    private String thesis;
+    public Phd(String programId, String name, int duration) {
+        super(programId, name, duration);
+    }
+
+    public void programProgression(Student student) {
+        int currentYear = student.getYearOfStudy();
+        if (currentYear < 4) {
+            System.out.println("PhD progression to the next year.");
+            student.setYearOfStudy(currentYear + 1);
+        } else {
+            System.out.println("Student has completed the PhD program.");
+        }
+    }
+
+    public String programID() {
+        return getProgramId();
+    }
+
+    public void setThesis(String thesis) {
+        this.thesis = thesis;
+    }
+
+    public String getThesis() {
+        return thesis;
+    }
+}
