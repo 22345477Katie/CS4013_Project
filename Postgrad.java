dfea9512abb74866e466fd7ee1f508561ccfb881
RaidMouras <133544926+RaidMouras@users.noreply.github.com>
2023-11-15 00:09:29 +0000

    Create Postgrad.java

---
+++ Postgrad.java
@@ -0,0 +1,27 @@
+public class Postgrad extends Program {
+    private int year;
+    public Postgrad(String programId, String name, int duration) {
+        super(programId, name, "Postgraduate", duration);
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void programProgression(Student student) {
+        int currentYear = student.getYearOfStudy();
+        if (currentYear < 2) {
+            student.setYearOfStudy(currentYear + 1);
+        } else {
+            System.out.println("Student has completed the postgraduate program.");
+        }
+    }
+
+    public String programID() {
+        return getProgramId();
+    }
+}
