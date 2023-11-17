07795bff747fa1a86cfc3f27a4d5853930c50e91
S-ashton <150346923+S-ashton@users.noreply.github.com>
2023-11-17 14:20:25 +0000

    Update Department.java

--- Department.java
+++ Department.java
@@ -3,21 +3,20 @@ public class Department {
     String studentsResults ;
     int departmentId ;
     boolean progression ; 
-    //ArrayList<Faculty> staffInDepartment ? ;
+    
     
     //creating a department with a name and an id
     public Department (String departmentName, int departmentId){
         this.departmentName = departmentName;
         this.departmentId = departmentId;
     }
-    
+
+    //return id of department
     public int getDepartmentId(){
         return this.departmentId;
     }
     
-    //method to return whether a student is progressing or not, I think 
-    //this should be attached to program/course maybe, department can access
-    //this info but should it be part of department??
+    //return whether a student is progressing to the next year or not
     public boolean reviewProgression(double qca){
         if(qca >= 2.0){
             progression = true;
