e2f2d78e80fa5bd162b370e5d7a937d5bf03325c
22345477Katie <150158819+22345477Katie@users.noreply.github.com>
2023-11-18 14:57:36 +0000

    Add files via upload

--- Semester.java
+++ Semester.java
@@ -1,1 +1,17 @@
-
+public class Semester{
+    private double factor;
+    private int semester;
+    
+    public Semester(double factor, int semester){
+        this.factor = factor;
+        this.semester = semester;
+    }
+    
+    public int getSemester(){
+        return semester;
+    }
+    
+    public double getFactor(){
+        return factor;
+    }
+}
