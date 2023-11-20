39a8b19b1efa09f06f25dc32774e5b03c2974aad
22345477Katie <150158819+22345477Katie@users.noreply.github.com>
2023-11-20 17:38:30 +0000

    Update Transcript.java

--- Transcript.java
+++ Transcript.java
@@ -1,32 +1,80 @@
 import java.time.*;
 public class Transcript{
-    private double semesterQCA;
-    private double sessionQCA;
-    private String transcriptOutputIntro;
-    private String transcriptOutputBody;
-    private Student student;
+    private double semesterQCA; //QCA for one semester
+    private double sessionQCA; //QCA to date (may/may not include first year)
+    private String transcriptOutputIntro; //student info
+    private String transcriptOutputBody; //QCA info
+    private Student s; //student object the transcript applies to
+    private Semester[] sems; //array of all completed semesters by the student 
     
+    //Katie
+    //Passes in student object
     public Transcript(Student student){
-        this.student = student;
+        s = student;
     }
     
-    public double calcSemesterQCA(Student s, Semester sem){
-        Module[] modules = sem.getModules();
-        double sumQCS=0;
-        double sumAttHrs=0;
-        double sumNonQHrs=0;
+    //Katie
+    //Calculates QCA based on a single semester
+    public double calcSemesterQCA(Semester sem){
+        ArrayList<Module> module = sem.getModules();
+        double sumQCS = 0;
+        double sumAttHrs = 0;
+        double sumNonQHrs = 0;
         double QCA;
         for (int i=0; i<modules.length; i++){
-            sumQCS = sumQCS + modules[i].getQCS;
-            sumAttHrs = sumAttHrs + modules[i].getAttHrs;
-            sumNonQHrs = sumNonQHrs + modules[i].getNonQHrs;
+            sumQCS = sumQCS + (module.get(i).getQPV()*module.get(i).getCredits()*sem.getFactor());
+            sumAttHrs = sumAttHrs + (module.get(i).getCredits()*sem.getFactor());
+            sumNonQHrs = sumNonQHrs + module.get(i).getNonQHrs();
         }
         
         QCA = sumQCS/(sumAttHrs-sumNonQHrs);
+        return QCA;
     }
     
-    public double calcCumulativeQCA(){
-        
+    //Katie
+    //Retrieves details for each individual semester to be used in calculating session QCA
+    public double[] getQCADetails(Semester sem){
+        ArrayList<Module> module = sem.getModules();
+        double sumQCS = 0;
+        double sumAttHrs = 0;
+        double sumNonQHrs = 0;
+        double[] details = new double[3];
+        for (int i=0; i<modules.length; i++){
+            sumQCS = sumQCS + (module.get(i).getQPV()*module.get(i).getCredits()*sem.getFactor());
+            sumAttHrs = sumAttHrs + (module.get(i).getCredits()*sem.getFactor());
+            sumNonQHrs = sumNonQHrs + module.get(i).getNonQHrs();
+        }
+        details[0] = sumQCS;
+        details[1] = sumAttHrs;
+        details[2] = sumNonQHrs;
+        return details;
+    }
+    
+    //Katie
+    //Calculates session QCA (ie. total QCA up until this point), taking into account the fact that common entry courses do not count first year's QCA in the session count after 1st year
+    public double calcCumulativeQCA(Semester[] sems){
+        double sumQCS = 0;
+        double sumAttHrs = 0;
+        double sumNonQHrs = 0;
+        double[] details = new double[3];
+        double QCA;
+        //need to make sure the student is not still in first year
+        if(s.getProgram().getType()==true){ //&& s.getYear>1){ //common entry or not
+            for(int j = 0; j<2; j++){
+                details = getQCADetails(s, sems[i]);
+                sumQCS = sumQCS + details[0];
+                sumAttHrs = sumHrs + details[1];
+                sumNonQHrs = sumNonQHrs + details[2];
+            }
+        }
+        for(int i = 2; i<sems.length(); i++){
+             details = getQCADetails(s, sems[i]);
+             sumQCS = sumQCS + details[0];
+             sumAttHrs = sumHrs + details[1];
+             sumNonQHrs = sumNonQHrs + details[2];
+        }
+        QCA = sumQCS/(sumAttHrs-sumNonQHrs);
+        return QCA;
     }
     
     public String outputTranscript(){
