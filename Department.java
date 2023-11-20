421c4a086652b84bb4b5ec4f6f2124460c8b1816
S-ashton <150346923+S-ashton@users.noreply.github.com>
2023-11-20 12:32:57 +0000

    Update Department.java

--- Department.java
+++ Department.java
@@ -1,9 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+
+
+
+
 public class Department {
     private String departmentName ;
     private String studentsResults ;
     private int departmentId ;
     private boolean progression ; 
-    
+    private ArrayList<Program> programs ;
     
     //creating a department with a name and an id
     public Department (String departmentName, int departmentId){
@@ -29,4 +44,22 @@ public class Department {
     
     //return whether a student is graduating or not
     //list of programs in department
+
+    private static ArrayList<Program> programsInDepartment(String fileName){
+        ArrayList<Program> programs = new ArrayList<Program>();
+        Path pathToFile = Paths.get(fileName);
+
+        try (BufferedReader br = Files.newBufferedReader(pathToFile, StandardCharsets.US_ASCII)){
+            String firstLine = br.readLine();
+            while (firstLine != null){
+                String[] namesOfPrograms = firstLine.split(",");
+                programs.add(namesOfPrograms);
+                line = br.readLine();
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        return programs;
+
+    }
 }
