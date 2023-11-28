9d4f2514a976af6d23ab265d194ba70780da8770
22345477Katie <150158819+22345477Katie@users.noreply.github.com>
2023-11-28 16:35:38 +0000

    Update Faculty.java

--- Faculty.java
+++ Faculty.java
@@ -6,22 +6,16 @@ import java.util.List;
 public class Faculty {
     // Basic faculty attributes
     private String title;
+    private String name;
     private double salary;
     private String office;
-    private final List<Student> students;
-    private final List<String> facultyChoices;
-    private List<String[]> gradeScaleArray;
-    private List<String[]> resultsArray;  // Array to store results
 
     // Constructor to initialise a Faculty object
-    public Faculty(String title, double salary, String office) {
+    public Faculty(String title, String name, double salary, String office) {
         this.title = title;
+        this.name = name;
         this.salary = salary;
         this.office = office;
-        this.students = new ArrayList<>();
-        this.facultyChoices = new ArrayList<>();
-        this.resultsArray = new ArrayList<>();
-        this.gradeScaleArray = new ArrayList<>();
     }
 
 
@@ -54,48 +48,12 @@ public class Faculty {
         return students;
     }
 
-    public List<String> getFacultyChoices() {
-        return facultyChoices;
-    }
-
-    // Constructor to initialize Faculty with a GradeScale
-    // Logic for submitting module results for a student from a CSV file
-    public void readResultsFromCSV(String resultsCsvFilePath) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(resultsCsvFilePath))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                // Split the CSV line into an array
-                String[] data = line.split(",");
-                String studentId = data[0];
-                String moduleId = data[1];
-                String grade = data[2];
-
-                // Store the result in the array
-                String[] result = {studentId, moduleId, grade};
-                resultsArray.add(result);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+    public void addStudentResults(String fileName, String moduleID){
+        if (RecordSystem.getModules().hasKey(moduleID){
+            Module module = RecordSystem.getModules().get(moduleID);
+            module.setGrades(RecordSystem.setGrades(fileName));
+        }else{
+            System.out.println("Module with this ID does not exist");
         }
     }
-
-    /*/ Logic for passing grade scale through the command line interface
-    public void readGradeScaleFromCommandLine(String[] args) {
-        if (args.length < 2) {
-            System.out.println("Usage: java Faculty <resultsCsvFilePath> <gradeScaleCsvFilePath>");
-            System.exit(1);
-        }
-
-        String gradeScaleCsvFilePath = args[1];
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(gradeScaleCsvFilePath))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                // Split the CSV line into an array
-                String[] grade = line.split(",");
-                gradeScaleArray.add(grade);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }/*
-    }
+}
